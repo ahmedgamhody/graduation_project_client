2e223cfded991d41dab53b5040ae6856e361c148
gamhody <ahmedgamhody1@outlook.com>
2025-04-16 12:10:39 +0200

    add pagination by react query not by useEffect

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </Provider>
 );
--- src/pages/homePage/AllPlaces/AllPlacesList.tsx
+++ src/pages/homePage/AllPlaces/AllPlacesList.tsx
@@ -1,52 +1,43 @@
 import { TPlaceHome } from "../../../types";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Pagination } from "flowbite-react";
 import { motion, AnimatePresence } from "framer-motion";
 import PlaceCard from "./PlaceCard";
 import CardPlaceSkeleton from "../../../animations/skeletons/CardPlaceSkeleton";
 import { useAppSelector } from "../../../store/hooks";
-
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 export default function AllPlacesList() {
-  const [places, setPlaces] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [pagesNumber, setPagesNumber] = useState(0);
   const token = useAppSelector((state) => state.auth.token);
-  const [loading, setLoading] = useState(false);
   const limitPerPage = 9;
   // Pagination logic
   const onPageChange = (page: number) => setCurrentPage(page);
 
-  useEffect(() => {
-    const fetchPlaces = async () => {
-      try {
-        setLoading(true);
-        const response = await axios(
-          `https://localhost:7214/api/Place/DisplayAllPlacesByPagnation?pageNumber=${currentPage}&pageSize=${limitPerPage}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        const data = response.data;
-        setPlaces(data.items);
-        setPagesNumber(data.totalPages);
-        console.log("Places data:", data.items);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching places:", error);
-        setLoading(false);
-      } finally {
-        setLoading(false);
+  const fetchAllPlaces = (currentPage = 0) =>
+    axios(
+      `https://localhost:7214/api/Place/DisplayAllPlacesByPagnation?pageNumber=${currentPage}&pageSize=${limitPerPage}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
-    };
-    fetchPlaces();
-  }, [token, currentPage]);
+    ).then((res) => {
+      const data = res.data;
+      setPagesNumber(data.totalPages);
+      return data;
+    });
+
+  const { data, isPlaceholderData } = useQuery({
+    queryKey: ["AllPlaces", currentPage],
+    queryFn: () => fetchAllPlaces(currentPage),
+    placeholderData: keepPreviousData,
+  });
 
   return (
     <div className="container mx-auto mt-10 flex flex-col items-center justify-center">
-      {loading ? (
+      {isPlaceholderData ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {Array.from({ length: 6 }, (_, index) => (
             <CardPlaceSkeleton key={index} />
@@ -63,7 +54,7 @@ export default function AllPlacesList() {
               transition={{ duration: 0.3 }}
               className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
             >
-              {places?.map((place: TPlaceHome, index) => (
+              {data?.items.map((place: TPlaceHome, index: number) => (
                 <PlaceCard place={place} key={place.id || index} />
               ))}
             </motion.div>
