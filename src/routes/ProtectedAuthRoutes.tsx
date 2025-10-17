260c8c2174b6ac44eadc59026e74fd997705ae5e
Ahmed Shaban Gamhody <130632647+ahmedgamhody@users.noreply.github.com>
2025-10-17 15:46:32 +0300

    Replace Navigate with useNavigate in ProtectedAuthRoutes
    Refactor ProtectedAuthRoutes to use useNavigate for navigation.

--- src/routes/ProtectedAuthRoutes.tsx
+++ src/routes/ProtectedAuthRoutes.tsx
@@ -1,12 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { useAppSelector } from "../store/hooks";
 
 export default function ProtectedAuthRoutes() {
   const { token } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
 
-  if (token) {
-    return <Navigate to="/" replace />;
-  }
+  useEffect(() => {
+    if (token) {
+      navigate(-1); // يرجع المستخدم خطوة للخلف
+    }
+  }, [token, navigate]);
 
   return <Outlet />;
 }
